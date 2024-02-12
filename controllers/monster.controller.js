3c90fcc4e0ea9efedc8bd6028abbe9d0c9e11edc
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-12 15:13:02 -0700

    Update error handling display to user

--- controllers/monster.controller.js
+++ controllers/monster.controller.js
@@ -1,61 +1,81 @@
 import { getMonstersFromRepo,getMonsterFromRepo, updateMonsterInRepo, deleteMonsterInRepo, createMonsterInRepo } from '../repositories/monster.repo.js';
 
-// Gets all the monsters list from database
+// Handle 404 errors to show user when a monster is not found
+const handleMonsterNotFound = (res, id) => {
+  res.status(404).send(`Monster ${id} not found`);
+};
+
+// Gets all the monsters list from the database
 export const getMonsters = async (req, res) => {
   try {
     const monsters = await getMonstersFromRepo();
     res.status(200).send(monsters);
   } catch (e) {
-    res.status(500).send(e.message, 'failed to fetch a list of monsters');
+    res.status(500).send(`Failed to fetch a list of monsters: ${e.message}`);
   }
 }
 
-// Gets a single monster by ID from the list in the database
+// Gets a single monster by ID from the database
 export const getMonster = async (req, res) => {
   const { id } = req.params;
   try {
     const monster = await getMonsterFromRepo({ id: id });
-    res.status(200).send(monster);
+    if (!monster) {
+      handleMonsterNotFound(res, id);
+    } else {
+      res.status(200).send(monster);
+    }
   } catch (e) {
-    res.status(500).send(e.message, `failed to fetch monster ${id}`);
+    res.status(500).send(`Failed to fetch monster ${id}: ${e.message}`);
   }
 }
 
-// Updates a single monster by ID from the list in the database
+// Updates a single monster by ID in the database
 export const updateMonster = async (req, res) => {
   const { id } = req.params;
   const { body } = req;
   try {
     const monster = await updateMonsterInRepo(id, body);
-    res.status(200).send(monster);
+    if (!monster) {
+      handleMonsterNotFound(res, id);
+    } else {
+      res.status(200).send(monster);
+    }
   } catch (e) {
-    res.status(500).send(e.message, `failed to fetch monster ${id}`);
+    res.status(500).send(`Failed to update monster ${id}: ${e.message}`);
   }
 }
 
-// Deletes a single monster by ID from the list in the database
+// Deletes a single monster by ID from the database
 export const deleteMonster = async (req, res) => {
   const { id } = req.params;
   try {
     const monster = await deleteMonsterInRepo({ id: id });
     if (monster) {
-      res.status(204).send();
+      if (!monster) {
+        handleMonsterNotFound(res, id);
+      } else {
+        res.status(204).send(monster);
+      }
     } else {
-      res.status(404).send(e.message, `failed to delete monster ${id}`);
+      res.status(404).send(`Failed to delete monster ${id}: ${e.message}`);
     };
   } catch (e) {
-    res.status(500).send(e.message, `failed to delete monster ${id}`);
+    res.status(500).send(`Failed to delete monster ${id}: ${e.message}`);
   }
 };
 
-// Createss a single monster in the database
+// Creates a single monster in the database
 export const createMonster = async (req, res) => {
   const { body } = req;
   try {
     const monster = await createMonsterInRepo(body);
-    console.log(monster);
-    res.status(200).send(monster);
+    if (!monster) {
+      handleMonsterNotFound(res, id);
+    } else {
+      res.status(200).send(monster);
+    }
   } catch (e) {
-    res.status(500).send(e.message, `failed to create monster`);
+    res.status(500).send(`Failed to create monster: ${e.message}`);
   }
 }

c025924142da5e696db754224df558e3a413eafe
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-12 15:27:16 -0700

    Add auto ID increment and better error response

--- repositories/monster.repo.js
+++ repositories/monster.repo.js
@@ -1,4 +1,5 @@
 import Monster from "../models/monster.model.js";
+import Counter from "../models/counter.model.js";
 
 export const getMonstersFromRepo = async (query) => {
   try {
@@ -14,20 +15,16 @@ export const getMonsterFromRepo = async (query) => {
     const monster = await Monster.findOne(query);
     return monster;
   } catch (e) {
-    throw Error(`Error while fetching monster with ID ${id}`);
+    throw Error(`Error while fetching monster: ${id}`);
   }
 }
 
 export const updateMonsterInRepo = async (id, update) => {
   try {
-    const monster = await Monster.findOneAndUpdate(
-      { id:id },
-      { ...update },
-      { new: true }
-    ).lean();
+    const monster = await Monster.findOneAndUpdate({ id:id }, { ...update }, { new: true } ).lean();
     return monster;
   } catch (e) {
-    throw Error("Error while updating monster");
+    throw Error(`Error while updating monster: ${id}`);
   } 
 }
 
@@ -36,16 +33,31 @@ export const deleteMonsterInRepo = async (query) => {
     const monster = await Monster.findOneAndDelete({ ...query });
     return monster;
   } catch (e) {
-    throw Error("Error while deleting a monster");
+    throw Error(`Error while deleting a monster: ${id}`);
   }
 }
 
 export const createMonsterInRepo = async (payload) => {
   try {
-    const newMonster = new Monster(payload);
+    // create a new monster object and save to database
+    const newMonster = new Monster({ ...payload });
     const savedMonster = await newMonster.save();
+
+    // auto increment ID
+    const counter = await Counter.findByIdAndUpdate({ _id: 'monsterId' }, { $inc: { seq: 1 } }, { new: true, upsert: true });
+
+    // update the saved monster ID with new increment and save again
+    savedMonster.id = counter.seq;
+    await savedMonster.save();
+
     return savedMonster;
   } catch (e) {
+    // throw proper error if monster ID already exists 
+    const existingMonster = await Monster.findOne({ id: payload.id });
+    if (existingMonster) {
+      throw new Error("Monster ID already exists");
+    }
+    // general error
     throw Error("Error while creating a monster");
   }
 }
